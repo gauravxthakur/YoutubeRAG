6d92d290631b9fe75cf040555478b5c87398a89c
gxt <gauravkthakur2002@gmail.com>
2025-09-10 03:45:56 +0530

    define the output parser and chain it with the model

--- rag.py
+++ rag.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
 #from langchain_core.prompts import ChatPromptTemplate
-#from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 #from langchain_core.runnables import RunnablePassthrough
 #from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain_openai import OpenAIEmbeddings
@@ -23,4 +23,9 @@ YOUTUBE_VIDEO = "https://youtu.be/dViKieKCM7o?si=fOMHR2oxIqJA4lGV"
 # Setup the model
 model = ChatOpenAI(model="deepseek/deepseek-chat-v3.1:free", openai_api_key=OPENAI_API_KEY, base_url="https://openrouter.ai/api/v1")
 
-print(model.invoke("who is Julius Caesar?"))
+# Define the parser
+parser = StrOutputParser()
+
+chain = model | parser
+
+print(chain.invoke("who is Julius Caesar?"))
